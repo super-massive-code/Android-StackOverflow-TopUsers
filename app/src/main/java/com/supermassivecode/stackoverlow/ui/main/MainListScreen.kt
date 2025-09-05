87e271777c9314186b1b371844e5c535f03c1ca8
carl <carltayloruk@protonmail.com>
2025-09-05 14:31:16 +0100

    Fix list not scrolling

--- app/src/main/java/com/supermassivecode/stackoverlow/ui/main/MainListScreen.kt
+++ app/src/main/java/com/supermassivecode/stackoverlow/ui/main/MainListScreen.kt
@@ -1,8 +1,12 @@
 package com.supermassivecode.stackoverlow.ui.main
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -11,9 +15,16 @@ fun  MainList(
     onFollowToggle: (UiUser) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column {
-        userList.forEach { userItem ->
-            UserListItem(userItem, onFollowToggle, modifier)
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        contentPadding = PaddingValues(vertical = 8.dp)
+    ) {
+        items(userList) { user ->
+            UserListItem(
+                user = user,
+                onFollowToggle = onFollowToggle
+            )
         }
     }
 }
